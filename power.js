691c6e34e946348f6a812d47298edc83201110ca
rahulroy177602 <rahulroy177602@gmail.com>
2021-12-30 22:45:04 +0530

    light theme added checkpoint for the previous commit becouse prevoius commit of this is good

---
+++ power.js
@@ -0,0 +1,54 @@
+
+function myFunction() {
+
+    var image = document.getElementById('icon');
+    if (image.src.match("./images/sun.png")) {
+        image.src = "./images/moon.png";
+        var wrapper = document.querySelectorAll(".change-bg");
+        wrapper[0].style.backgroundColor = "white";
+       
+       
+        var links = document.querySelectorAll(".link");
+        for(var i = 0;i<links.length;i++){
+            links[i].style.color ="black";
+        }
+
+        
+        var x = document.querySelectorAll(".black-color");
+        for (var i = 0; i < x.length; i++) {
+            x[i].style.color = "black";
+        }
+
+        var adminName = document.querySelectorAll(".admin-name");
+        for (var i = 0; i < adminName.length; i++) {
+            adminName[i].style.color = "#3D405B";
+        }
+      
+    }
+    else {
+        image.src = "./images/sun.png";
+        var wrapper = document.getElementsByClassName("change-bg");
+        wrapper[0].style.backgroundColor = " rgb(34, 34, 34)";
+        var element = document.getElementsByClassName("link");
+        var adminName = document.getElementsByClassName("admin-name");
+        var black_color = document.getElementsByClassName("black-color");
+
+        for (var i = 0; i < element.length; i++) {
+            element[i].style.color = "white";
+        }
+        for (var i = 0; i < adminName.length; i++) {
+            adminName[i].style.color = "white";
+        }
+        for (var i = 0; i < black_color.length; i++) {
+            black_color[i].style.color = "white";
+        }
+
+        
+
+
+    }
+
+       
+
+
+}
